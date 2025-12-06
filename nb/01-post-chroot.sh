1c020dd0fb39a7c1b5510f51806443d89deaae9d
Matthias Schabhüttl <63157170+mschabhuettl@users.noreply.github.com>
2025-12-06 20:48:12 +0100

    Remove NTP setup from post-chroot script
    Removed pre-setup steps for timezone and NTP configuration.

--- nb/01-post-chroot.sh
+++ nb/01-post-chroot.sh
@@ -8,13 +8,6 @@ verbose() {
   echo -e "\033[1;32m[INFO]\033[0m $1"
 }
 
-# Pre-setup: Set timezone and NTP
-verbose "Setting up timezone and NTP."
-timedatectl set-timezone Europe/Vienna
-verbose "Timezone set to Europe/Vienna."
-timedatectl set-ntp true
-verbose "NTP enabled."
-
 # Set timezone and hardware clock
 verbose "Setting timezone and hardware clock."
 ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime
