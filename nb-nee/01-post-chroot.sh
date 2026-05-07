9c14eafdc5a5a1c86f6aebca4b91d857723cac61
Matthias Schabhüttl <63157170+mschabhuettl@users.noreply.github.com>
2026-05-07 22:42:51 +0200

    Update 01-post-chroot.sh

--- nb-nee/01-post-chroot.sh
+++ nb-nee/01-post-chroot.sh
@@ -108,7 +108,7 @@ verbose "Package database refreshed."
 
 # Install essential packages
 verbose "Installing essential packages."
-pacman -S plasma-meta kde-graphics-meta kde-multimedia-meta kde-network-meta kde-sdk-meta kde-system-meta kde-utilities-meta ttf-dejavu breeze xdg-user-dirs sddm pulseaudio pulseaudio-bluetooth firewalld ipset thunderbird firefox keepassxc bluez bluez-utils networkmanager-vpnc htop gimp vlc libreoffice-fresh gparted ntfs-3g git nm-connection-editor acpid dbus avahi cups nss-mdns chrony qt5-wayland android-tools scrcpy system-config-printer dosfstools tree tlp xdg-desktop-portal efibootmgr plymouth sof-firmware
+pacman -S plasma-meta kde-graphics-meta kde-multimedia-meta kde-network-meta kde-sdk-meta kde-system-meta kde-utilities-meta ttf-dejavu breeze xdg-user-dirs plasma-login-manager pulseaudio pulseaudio-bluetooth firewalld ipset thunderbird firefox keepassxc bluez bluez-utils networkmanager-vpnc htop gimp vlc libreoffice-fresh gparted ntfs-3g git nm-connection-editor acpid dbus avahi cups nss-mdns chrony qt5-wayland android-tools scrcpy system-config-printer dosfstools tree tlp xdg-desktop-portal efibootmgr plymouth rsync networkmanager-openconnect efitools sbsigntools sbctl tcpdump usbutils cdrtools libisoburn cdrdao libburn dvd+rw-tools ccd2iso sox libcdio audiocd-kio k3b libdvdread libdvdcss libdvdnav libaacs libbluray wget sof-firmware
 verbose "Essential packages installed."
 
 # Configure services
@@ -150,8 +150,11 @@ verbose "Installing Mesa drivers."
 pacman -S mesa
 verbose "Mesa drivers installed."
 
-sed -i 's/^MODULES=.*/MODULES=(i915)/' /etc/mkinitcpio.conf
+# Adding MODULES to initramfs
+verbose "Adding MODULES to initramfs."
+sed -i 's/^MODULES=.*/MODULES=(i915 vfat fat)/' /etc/mkinitcpio.conf
 mkinitcpio -P
+verbose "MODULES added."
 
 # Install additional packages
 verbose "Installing additional packages."
